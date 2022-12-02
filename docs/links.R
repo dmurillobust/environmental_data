e2a454904fbb51ac9cc7f1cd967f9f59c8002dbf
dmurillobust <117607630+dmurillobust@users.noreply.github.com>
2022-12-02 17:22:03 -0500

    Add files via upload

---
+++ docs/links.R
@@ -0,0 +1,33 @@
+mk_link = function(
+  file_name, lnk_txt,
+  html_path, lnk_fmt = "[%s](%s)")
+{
+  (sprintf(
+    lnk_fmt, lnk_txt, 
+    file.path(html_path, file_name)))
+}
+
+
+
+
+# ---- Webpages ----
+{
+  datacamp_url = "https://www.datacamp.com/"
+  github_url = "https://www.github.com"
+  rstudio_url = "https://www.rstudio.com"
+  r_url = "https://cran.r-project.org/"
+  
+  mcgarigal_landscape_eco = "https://www.umass.edu/landeco/"
+  
+  umass_url = "https://umass.edu"
+  eco_dept_url = "https://eco.umass.edu/"
+  
+  umass_v_desktop = "https://www.umass.edu/it/computer-classrooms/windows-virtual-desktop"
+  umass_online_storage = "https://www.umass.edu/it/online-storage-collaboration"
+  
+  
+  
+  
+}
+
+
